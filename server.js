a38085f03b8d8968b635a419777926e727153a65
Lily Russ <llyruss@yahoo.com>
2023-01-16 17:55:53 -0600

    updated server file

--- server.js
+++ server.js
@@ -1,28 +1,27 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const path = require('path')
-const routes = require('./routes')
+const express = require("express")
+const mongoose = require("mongoose")
+const path = require("path")
+const routes = require("./routes")
 
 
-const app = express()
+
 const PORT = process.env.PORT || 3000
+const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-const mongodbURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/'
 
 mongoose.connect(
-    mongodbURI + 'socialDB', 
-    {
+    process.env.MONGODB_URI || "mongodb://localhost/twitterDB", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // useCreateIndex: true,
+    // useFindAndModify: false
+  });
 
-    }
-)
-
-
-
-app.use(express.urlencoded({extended: true} ));
-app.use(express.json());
-app.use(routes)
+  app.use(routes)
 
 app.listen(PORT, () => {
-    console.log(`server is running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`)
 })
